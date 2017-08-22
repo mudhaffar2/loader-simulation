d6309567f59ac75657539a250a0cc78dccb3211b
mudhafar <mudhaffar2@gmail.com>
2017-08-22 10:12:06 +0200

    Add some JavaScript

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 var simulateProgress = require('simulate-progress');
+var loaderEl = document.querySelector('.loader');
+var progressEl = document.querySelector('.progress');
+function progressCallback(percent) {
+
+  progressEl.style.width = percent;
+
+}
+
+function finishCallback() {
+
+  loaderEl.classList.addClass('finished');
+
+
+}
+
+function startSimulation() {
+
+  loaderEl.classList.removeClass('finished');
+
+  simulateProgress(progressCallback ,finishCallback);
+
+
+}
+
+startSimulation()
+
+
+
